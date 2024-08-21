23c77c8d58a146dfd53d37a23fa7928e23ea2ac8
AlexSanchez <alexander.snchz07@gmail.com>
2024-08-21 17:17:34 -0500

    Updates

---
+++ assets/RecordPersonal-DWIKXUsL.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-C-FsKAAJ.js";function t(){return o.jsx("div",{className:"container mx-auto"})}export{t as default};
--- assets/RecordPersonal-aBn1-NuY.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-tczcSmrJ.js";function t(){return o.jsx("div",{className:"container mx-auto"})}export{t as default};
